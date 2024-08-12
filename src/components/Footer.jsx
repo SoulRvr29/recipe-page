7ce1bdadc9d11ca38fa6a5603978a43882713b47
Paweł Chudecki <pawelc29@interia.pl>
2024-08-12 10:04:59 +0200

    footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer>
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by
+      <a href="https://www.frontendmentor.io/profile/SoulRvr29" target="_blank">
+        Paweł Chudecki
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;
